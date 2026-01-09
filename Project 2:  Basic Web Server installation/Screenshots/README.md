e251d6cd32a008977c26a21e3d32d4d2f59900c2
mahmouddepian457 <mahmouddepian7@gmail.com>
2026-01-09 18:18:59 +0200

    Delete Project 2:  Basic Web Server installation directory

--- Project 2:  Basic Web Server installation/Screenshots/README.md
+++
@@ -1,1 +0,0 @@
-this photo appear all steeps we have done from install nginx web server and make it enable and check logs
--- Project 2:  Basic Web Server installation/Scripts/checkwebserver.sh
+++
@@ -1,6 +0,0 @@
-if systemctl is-active --quiet nginx; then
-    echo "✅ Web Server is Running"
-else
-    echo "❌ Web Server is NOT Running. Restarting..."
-    sudo systemctl restart nginx
-fi
